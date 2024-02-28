d3fa31d6e5fda8a251202dbecaf7055301d0f0f0
heitor lourenco <juniorio01@hotmail.com>
2024-02-28 19:19:03 -0300

    feat: add table in feature/create-table

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
 export default function Home() {
-  return <></>;
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-white">
+      <div className="flex shadow-xl rounded-lg">
+        <div className="w-[287px] h-[380px]">
+          <div className="p-4 bg-gray-50">
+            <h2 className="text-gray-500 font-semibold text-xs">CAR</h2>
+          </div>
+        </div>
+        <div className="w-[287px] h-[380px]">
+          <div className="p-4 bg-gray-50">
+            <h2 className="text-gray-500 font-semibold text-xs">
+              NEXT RESERVATION
+            </h2>
+          </div>
+        </div>
+        <div className="w-[287px] h-[380px]">
+          <div className="p-4 bg-gray-50">
+            <h2 className="text-gray-500 font-semibold text-xs">STATUS</h2>
+          </div>
+        </div>
+        <div className="w-[287px] h-[380px]">
+          <div className="p-4 bg-gray-50">
+            <h2 className="text-gray-500 font-semibold text-xs">RATING</h2>
+          </div>
+        </div>
+        <div className="w-[91px] h-[380px]">
+          <div className="p-4 bg-gray-50">
+            <h2 className="text-gray-500 font-semibold text-xs">ACTIONS</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
